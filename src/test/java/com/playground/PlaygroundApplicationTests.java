81c00320dd8b9b76069514c0ee78415d13ab210e
AndrewJo <andrewjo123@hanmail.net>
2024-10-18 15:30:34 +0900

    테스트

--- src/test/java/com/playground/PlaygroundApplicationTests.java
+++ src/test/java/com/playground/PlaygroundApplicationTests.java
@@ -3,8 +3,6 @@ package com.playground;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PlaygroundApplicationTests {
 
 	@Test
 	void contextLoads() {
