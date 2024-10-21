189be2429d1b3f9393698a3abd2df1660bbd550f
AndrewJo <andrewjo123@hanmail.net>
2024-10-21 11:32:29 +0900

    빼먹은 파일 추가

--- src/main/java/com/playground/repository/MemberRepository.java
+++ src/main/java/com/playground/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import com.playground.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -12,6 +13,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("select m.email from Member m where m.name=:name and m.phone=:phone")
     Object[] findEmail(@Param("name") String name, @Param("phone")String phone);
 
+    Optional<Member> findByEmailAndFromSocial(String email, boolean fromSocial);
     //추가
     @Query("select m from Member m join Order o on o.member=m where o.id = :orderId")
     Member findBuyer(@Param("orderId") Long orderId);
