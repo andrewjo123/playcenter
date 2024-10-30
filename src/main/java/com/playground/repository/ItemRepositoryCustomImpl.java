645724ae186cb1a721c0920b65a85f25ecafbfed
DESKTOP-71VKKTV\14C <globalitmini@gmail.com>
2024-10-30 16:33:43 +0900

    main

--- src/main/java/com/playground/repository/ItemRepositoryCustomImpl.java
+++ src/main/java/com/playground/repository/ItemRepositoryCustomImpl.java
@@ -1,7 +1,11 @@
 package com.playground.repository;
 
+import com.playground.dto.ItemCategoryDto;
+import com.playground.entity.QItemCategory;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Wildcard;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.playground.constant.ItemSellStatus;
 import com.playground.dto.ItemSearchDto;
@@ -90,10 +94,43 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom{
     }
 
     @Override
-    public Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable) {
+    public Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, ItemCategoryDto itemCategoryDto,Pageable pageable) {
         QItem item = QItem.item;
         QItemImg itemImg = QItemImg.itemImg;
+        QItemCategory itemCategory= QItemCategory.itemCategory;
 
+        BooleanBuilder whereClause = new BooleanBuilder();
+
+        // 기본 조건 추가
+        whereClause.and(itemImg.repimgYn.eq("Y"));
+        whereClause.and(itemNmLike(itemSearchDto.getSearchQuery()));
+
+        // ItemCategoryDto의 필드 추가
+        if (itemCategoryDto != null) {
+            if (itemCategoryDto.isAction()) whereClause.and(itemCategory.action.eq(true));
+            if (itemCategoryDto.isAdventure()) whereClause.and(itemCategory.adventure.eq(true));
+            if (itemCategoryDto.isRpg()) whereClause.and(itemCategory.rpg.eq(true));
+            if (itemCategoryDto.isShooter()) whereClause.and(itemCategory.shooter.eq(true));
+            if (itemCategoryDto.isStrategy()) whereClause.and(itemCategory.strategy.eq(true));
+            if (itemCategoryDto.isSimulation()) whereClause.and(itemCategory.simulation.eq(true));
+            if (itemCategoryDto.isPuzzle()) whereClause.and(itemCategory.puzzle.eq(true));
+            if (itemCategoryDto.isSports()) whereClause.and(itemCategory.sports.eq(true));
+            if (itemCategoryDto.isRacing()) whereClause.and(itemCategory.racing.eq(true));
+            if (itemCategoryDto.isFighting()) whereClause.and(itemCategory.fighting.eq(true));
+            if (itemCategoryDto.isSurvival()) whereClause.and(itemCategory.survival.eq(true));
+            if (itemCategoryDto.isRhythm()) whereClause.and(itemCategory.rhythm.eq(true));
+            if (itemCategoryDto.isSandbox()) whereClause.and(itemCategory.sandbox.eq(true));
+            if (itemCategoryDto.isBattleRoyale()) whereClause.and(itemCategory.battleRoyale.eq(true));
+            if (itemCategoryDto.isCard()) whereClause.and(itemCategory.card.eq(true));
+            if (itemCategoryDto.isBoardGame()) whereClause.and(itemCategory.boardGame.eq(true));
+            if (itemCategoryDto.isHorror()) whereClause.and(itemCategory.horror.eq(true));
+            if (itemCategoryDto.isPlatformer()) whereClause.and(itemCategory.platformer.eq(true));
+            if (itemCategoryDto.isMoba()) whereClause.and(itemCategory.moba.eq(true));
+            if (itemCategoryDto.isMmorpg()) whereClause.and(itemCategory.mmorpg.eq(true));
+        }
+        System.out.println(itemCategoryDto.isAction());
+        System.out.println("::::::::::::::::::::::::::::::::::::::");
+        System.out.println(whereClause);
         List<MainItemDto> content = queryFactory
                 .select(
                         new QMainItemDto(
@@ -105,8 +142,8 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom{
                 )
                 .from(itemImg)
                 .join(itemImg.item, item)
-                .where(itemImg.repimgYn.eq("Y"))
-                .where(itemNmLike(itemSearchDto.getSearchQuery()))
+                .join(itemCategory).on(itemCategory.item.eq(item))
+                .where(whereClause)
                 .orderBy(item.id.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
@@ -116,11 +153,125 @@ public class ItemRepositoryCustomImpl implements ItemRepositoryCustom{
                 .select(Wildcard.count)
                 .from(itemImg)
                 .join(itemImg.item, item)
-                .where(itemImg.repimgYn.eq("Y"))
-                .where(itemNmLike(itemSearchDto.getSearchQuery()))
+                .join(itemCategory).on(itemCategory.item.eq(item))
+                .where(whereClause)
                 .fetchOne()
                 ;
 
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    @Override
+    public Page<MainItemDto> getMainItemPage2(String company, ItemSearchDto itemSearchDto, ItemCategoryDto itemCategoryDto,Pageable pageable) {
+        QItem item = QItem.item;
+        QItemImg itemImg = QItemImg.itemImg;
+        QItemCategory itemCategory= QItemCategory.itemCategory;
+
+        BooleanBuilder whereClause = new BooleanBuilder();
+
+        // 기본 조건 추가
+        whereClause.and(itemImg.repimgYn.eq("Y"));
+        whereClause.and(itemCategory.company.eq(company));
+        whereClause.and(itemNmLike(itemSearchDto.getSearchQuery()));
+
+        // ItemCategoryDto의 필드 추가
+        if (itemCategoryDto != null) {
+            if (itemCategoryDto.isAction()) whereClause.and(itemCategory.action.eq(true));
+            if (itemCategoryDto.isAdventure()) whereClause.and(itemCategory.adventure.eq(true));
+            if (itemCategoryDto.isRpg()) whereClause.and(itemCategory.rpg.eq(true));
+            if (itemCategoryDto.isShooter()) whereClause.and(itemCategory.shooter.eq(true));
+            if (itemCategoryDto.isStrategy()) whereClause.and(itemCategory.strategy.eq(true));
+            if (itemCategoryDto.isSimulation()) whereClause.and(itemCategory.simulation.eq(true));
+            if (itemCategoryDto.isPuzzle()) whereClause.and(itemCategory.puzzle.eq(true));
+            if (itemCategoryDto.isSports()) whereClause.and(itemCategory.sports.eq(true));
+            if (itemCategoryDto.isRacing()) whereClause.and(itemCategory.racing.eq(true));
+            if (itemCategoryDto.isFighting()) whereClause.and(itemCategory.fighting.eq(true));
+            if (itemCategoryDto.isSurvival()) whereClause.and(itemCategory.survival.eq(true));
+            if (itemCategoryDto.isRhythm()) whereClause.and(itemCategory.rhythm.eq(true));
+            if (itemCategoryDto.isSandbox()) whereClause.and(itemCategory.sandbox.eq(true));
+            if (itemCategoryDto.isBattleRoyale()) whereClause.and(itemCategory.battleRoyale.eq(true));
+            if (itemCategoryDto.isCard()) whereClause.and(itemCategory.card.eq(true));
+            if (itemCategoryDto.isBoardGame()) whereClause.and(itemCategory.boardGame.eq(true));
+            if (itemCategoryDto.isHorror()) whereClause.and(itemCategory.horror.eq(true));
+            if (itemCategoryDto.isPlatformer()) whereClause.and(itemCategory.platformer.eq(true));
+            if (itemCategoryDto.isMoba()) whereClause.and(itemCategory.moba.eq(true));
+            if (itemCategoryDto.isMmorpg()) whereClause.and(itemCategory.mmorpg.eq(true));
+        }
+
+        System.out.println("::::::::::::::::::::::::::::::::::::::");
+        System.out.println(whereClause);
+        List<MainItemDto> content = queryFactory
+                .select(
+                        new QMainItemDto(
+                                item.id,
+                                item.itemNm,
+                                item.itemDetail,
+                                itemImg.imgUrl,
+                                item.price)
+                )
+                .from(itemImg)
+                .join(itemImg.item, item)
+                .join(itemCategory).on(itemCategory.item.eq(item))
+                .where(whereClause)
+                .orderBy(item.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long total = queryFactory
+                .select(Wildcard.count)
+                .from(itemImg)
+                .join(itemImg.item, item)
+                .join(itemCategory).on(itemCategory.item.eq(item))
+                .where(whereClause)
+                .fetchOne();
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    @Override
+    public Page<MainItemDto> getMainItem(String company, ItemSearchDto itemSearchDto, ItemCategoryDto itemCategoryDto,Pageable pageable) {
+        QItem item = QItem.item;
+        QItemImg itemImg = QItemImg.itemImg;
+        QItemCategory itemCategory= QItemCategory.itemCategory;
+
+        BooleanBuilder whereClause = new BooleanBuilder();
+           JPAQuery<Item> query = queryFactory.selectFrom(item);
+
+       // 기본 조건 추가
+       whereClause.and(itemImg.repimgYn.eq("Y")); // 대표 이미지 조건
+   
+
+       if (company != null) {
+        whereClause.and(itemCategory.company.eq(company));
+        }
+    
+        List<MainItemDto> content = queryFactory
+                .select(
+                        new QMainItemDto(
+                                item.id,
+                                item.itemNm,
+                                item.itemDetail,
+                                itemImg.imgUrl,
+                                item.price)
+                )
+                .from(itemImg)
+                .join(itemImg.item, item)
+                .join(itemCategory).on(itemCategory.item.eq(item))
+                .where(whereClause)
+                .orderBy(item.openDate.desc(), item.id.desc()) 
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    
+        long total = queryFactory
+                .select(Wildcard.count)
+                .from(itemImg)
+                .join(itemImg.item, item)
+                .join(itemCategory).on(itemCategory.item.eq(item))
+                .where(whereClause)
+                .fetchOne();
+    
         return new PageImpl<>(content, pageable, total);
     }
 
--- src/main/java/com/playground/service/ItemServiceImpl.java
+++ src/main/java/com/playground/service/ItemServiceImpl.java
@@ -1,15 +1,12 @@
 package com.playground.service;
 
-import com.playground.dto.ItemFormDto;
-import com.playground.dto.ItemImgDto;
-import com.playground.dto.ItemSearchDto;
-import com.playground.dto.MainItemDto;
+import com.playground.dto.*;
 import com.playground.entity.Item;
 import com.playground.entity.ItemCategory;
+import com.playground.entity.ItemCode;
 import com.playground.entity.ItemImg;
-import com.playground.repository.ItemCategoryRepository;
-import com.playground.repository.ItemImgRepository;
-import com.playground.repository.ItemRepository;
+import com.playground.repository.*;
+
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -18,8 +15,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import javax.print.DocPrintJob;
+
+import static com.playground.dto.ItemCategoryDto.modelMapper;
 
 @Service
 @Transactional
@@ -30,6 +37,9 @@ public class ItemServiceImpl implements ItemService {
     private final ItemImgService itemImgService;
     private final ItemImgRepository itemImgRepository;
     private final ItemCategoryRepository categoryRepository;
+    private final ItemCodeRepository itemCodeRepository;
+
+
 
     @Override
     @Transactional
@@ -38,11 +48,7 @@ public class ItemServiceImpl implements ItemService {
         Item item = itemFormDto.createItem();
         itemRepository.save(item);
 
-        // 추가
-        ItemCategory category=new ItemCategory();
-        category.setCompany(itemFormDto.getCompany());
-        category.setTag(itemFormDto.getTag());
-        category.setItem(item);
+        ItemCategory category=itemFormDto.getItemCategoryDto().toEntity(item, itemFormDto.getCompany());
         categoryRepository.save(category);
 
         // Register images
@@ -57,8 +63,7 @@ public class ItemServiceImpl implements ItemService {
 
         return item.getId();
     }
-
-    @Transactional(readOnly = true)
+  
     @Override
     public ItemFormDto getItemDtl(Long itemId) {
         List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
@@ -75,7 +80,29 @@ public class ItemServiceImpl implements ItemService {
         //추가
         ItemCategory categories=categoryRepository.findByItemId(itemId);
         itemFormDto.setCompany(categories.getCompany());
-        itemFormDto.setTag(categories.getTag());
+        ItemCategoryDto itemCategoryDto=ItemCategoryDto.fromEntity(categories);
+        itemFormDto.setItemCategoryDto(itemCategoryDto);
+        
+        // 리뷰 추가
+        List<Object[]>result=itemRepository.getAvgAndCount(itemId);
+
+        if (result != null && result.get(0).length == 2) {
+            Double avg = (Double) result.get(0)[0]; // Average grade
+            Long allReview = (Long) result.get(0)[1];   // Count of reviews
+            
+            System.out.println("Average: " + avg);
+            System.out.println("Count: " + allReview);
+
+            itemFormDto.setAvg(avg);
+            itemFormDto.setAllReview(allReview);
+        } else {
+            // Handle cases where there are no reviews or the item does not exist
+            System.out.println("No reviews found or item does not exist.");
+            itemFormDto.setAvg(0D);
+            itemFormDto.setAllReview(0L);
+        }
+
+        
         return itemFormDto;
     }
 
@@ -95,8 +122,8 @@ public class ItemServiceImpl implements ItemService {
 
         //추가
         ItemCategory category=categoryRepository.findByItemId(item.getId());
+        modelMapper.map(itemFormDto.getItemCategoryDto(),category);
         category.setCompany(itemFormDto.getCompany());
-        category.setTag(itemFormDto.getTag());
         categoryRepository.save(category);
 
         return item.getId();
@@ -110,7 +137,38 @@ public class ItemServiceImpl implements ItemService {
 
     @Transactional(readOnly = true)
     @Override
-    public Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable) {
-        return itemRepository.getMainItemPage(itemSearchDto, pageable);
+    public Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, ItemCategoryDto itemCategoryDto, Pageable pageable) {
+        return itemRepository.getMainItemPage(itemSearchDto, itemCategoryDto, pageable);
+    }
+
+    @Override
+    public Page<MainItemDto> getMainItemPage2(String company, ItemSearchDto itemSearchDto, ItemCategoryDto itemCategoryDto,Pageable pageable) {
+        return itemRepository.getMainItemPage2(company, itemSearchDto, itemCategoryDto, pageable);
+    }
+
+    // 1024추가
+    @Transactional
+    @Override
+    public int saveCodes(Long itemId, List<String> codes) {
+        Item item = itemRepository.findById(itemId).get();
+        List<ItemCode> itemCodes = new ArrayList<>();
+
+        codes.forEach(code -> {
+            ItemCode itemCode = new ItemCode();
+            itemCode.setItem(item);
+            itemCode.setCodNum(code);
+            itemCodes.add(itemCode);
+        });
+
+        itemCodeRepository.saveAll(itemCodes);
+        item.setStockNumber(item.getStockNumber()+codes.size());
+        itemRepository.save(item);
+        return codes.size(); // 반복한 횟수 반환
+    }
+
+
+    @Override
+    public Page<MainItemDto> getMainItem(String company, ItemSearchDto itemSearchDto, ItemCategoryDto itemCategoryDto, Pageable pageable) {
+        return itemRepository.getMainItem(company, itemSearchDto, itemCategoryDto, pageable); // Repository 메서드 호출
     }
 }
